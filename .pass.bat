5858e434f5a7d5abd3a4b71f697dbcfe967326fb
anlaki <91802653+anlaki-py@users.noreply.github.com>
2024-05-12 13:48:23 +0100

    Update .pass.bat
    need some tweeks

--- .pass.bat
+++ .pass.bat
@@ -9,20 +9,16 @@ set log="python\pass\pass.txt"
 IF "%1"=="cat" (
     IF EXIST %log% (
         type %log%
-        exit
     ) ELSE (
         echo Error: File is empty or not found.
-        exit
     )
 ) ELSE IF "%1"=="clear" (
     IF EXIST %log% (
         del %log%
         echo => The log file has been successfully cleared.
-        exit
     ) ELSE (
         echo Error: File is already cleared or deleted.
-        exit
     )
 )
 
-python %py%
+py %py%
