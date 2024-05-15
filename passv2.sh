34c0af3096d1e60eb17ba1b2cb6a5c9fbb941496
anlaki <91802653+anlaki-py@users.noreply.github.com>
2024-05-15 17:24:03 +0100

    v2 test

---
+++ passv2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#python file path
+py="$HOME/pass-gen/python/pass/passv2.py"
+
+#log file path
+log="$HOME/pass-gen/python/pass/pass.log"
+
+#help file path
+help="$HOME/pass-gen/python/pass/help.txt"
+
+if [ "$1" = "cat" ]; then
+    if [ -f $log ]; then
+        less $log
+        exit
+    else
+        echo Error: File is empty or not found.
+        exit
+    fi
+elif [ "$1" = "clear" ]; then
+    if [ -f $log ]; then
+        rm $log
+        echo The log file has been successfully cleared.
+        exit
+    else
+        echo Error: File is already cleared or deleted.
+        exit
+    fi
+elif [ "$1" = "help" ]; then
+    if [ -f $help ]; then
+	less $help
+	exit
+    else
+	echo Error: file not found.
+        exit	
+    fi
+elif [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <length>"
+    python3 $py "$1"
+    exit 1
+fi
+
+    
