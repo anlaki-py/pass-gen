15b44351080039cf2accab3b406c0f12c0ee23ee
anlaki <91802653+anlaki-py@users.noreply.github.com>
2024-06-30 16:08:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,31 @@
 
 set -e
 
+# Function to remove pass file
+remove_pass_file() {
+    rm /usr/local/bin/pass
+    echo "Existing pass file removed."
+}
+
+# Function to download and install the pass file
+install_pass_file() {
+    echo "Downloading new pass file..."
+    curl -sSL -o pass https://raw.githubusercontent.com/anlaki-py/pass-gen/main/pass
+    chmod +x pass
+    mv pass /usr/local/bin/
+    echo "The pass executable has been successfully moved to the PATH."
+}
+
 if [ -f /usr/local/bin/pass ]; then
     read -p "The pass file already exists. Do you want to remove it? (y/n): " choice
     case "$choice" in 
-      y|Y ) rm /usr/local/bin/pass
-            echo "Existing pass file removed.";;
+      y|Y ) remove_pass_file
+            install_pass_file;;
       n|N ) echo "Existing pass file retained. Exiting script."
             exit 0;;
       * ) echo "Invalid input. Exiting script."
           exit 1;;
     esac
+else
+    install_pass_file
 fi
-
-echo "Downloading new pass file..."
-curl -sSL -o pass https://raw.githubusercontent.com/anlaki-py/pass-gen/main/pass
-chmod +x pass
-mv pass /usr/local/bin/
-echo "The pass executable has been successfully moved to the PATH."
