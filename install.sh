5d4bee77cd1f50bb47dfb1181a576415754afa76
anlaki <91802653+anlaki-py@users.noreply.github.com>
2024-06-30 02:46:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-# Download the pass executable from GitHub
-curl -sSL -o pass https://raw.githubusercontent.com/anlaki-py/pass-gen/main/pass
-
-# Make the downloaded file executable
-chmod +x pass
-
-# Move the pass executable to a directory in your $PATH
-mv pass /usr/local/bin/
-
-echo "The pass executable has been successfully moved to the PATH."
+# Check if the pass executable already exists
+if ! command -v pass &> /dev/null; then
+    # Download the pass executable from GitHub
+    if curl -sSL -o pass https://raw.githubusercontent.com/anlaki-py/pass-gen/main/pass; then
+        # Make the downloaded file executable
+        if chmod +x pass; then
+            # Move the pass executable to a directory in your $PATH
+            if mv pass /usr/local/bin/; then
+                echo "The pass executable has been successfully moved to the PATH."
+            else
+                echo "Failed to move the pass executable to /usr/local/bin/"
+                exit 1
+            fi
+        else
+            echo "Failed to make the pass file executable."
+            exit 1
+        fi
+    else
+        echo "Failed to download the pass executable."
+        exit 1
+    fi
+else
+    echo "The pass executable already exists in the PATH. Skipping download."
+fi
